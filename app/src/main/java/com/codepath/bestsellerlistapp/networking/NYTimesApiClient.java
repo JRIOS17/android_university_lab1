0a440d5c0e557576f3db0d514f2d49df8bb585b5
JRIOS17 <jessiearocherios70@gmail.com>
2021-09-02 10:13:58 -0400

    added my unique key

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "4FiOu4vTcUV8pGV6OLsqn046lV7v3ogX";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
